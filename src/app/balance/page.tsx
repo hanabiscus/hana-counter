376a59343a49b5b0caa60e68d76b9cdd7715eac8
hanabiscus <hana-counter@outlook.jp>
2025-04-17 03:31:35 +0900

    収支ページ認証修正

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,10 +1,29 @@
 import Balance from "@/components/page-components/Balance";
+import { checkAuthSession } from "@/server/authentication/amplifyAuthSession";
 import { getMonthlyBalanceData } from "@/server/balance/balanceProcessors";
+import { balanceDTOType } from "@/const/types";
 
 const BalancePage = async () => {
-  const fetchedMonthlyBalanceData = await getMonthlyBalanceData("2025-04");
+  const isLogin = await checkAuthSession();
+  const fetchedMonthlyBalanceData: balanceDTOType = [];
+  if (isLogin) {
+    Array.prototype.push.apply(
+      fetchedMonthlyBalanceData,
+      await getMonthlyBalanceData("2025-04")
+    );
+  }
 
-  return <Balance balanceDataDTO={fetchedMonthlyBalanceData} />;
+  return (
+    <>
+      {isLogin ? (
+        <Balance balanceDataDTO={fetchedMonthlyBalanceData} />
+      ) : (
+        <div className="h-svh flex justify-center items-center">
+          <Balance balanceDataDTO={fetchedMonthlyBalanceData} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default BalancePage;
---
+++ src/server/authentication/amplifyAuthSession.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { Amplify } from "aws-amplify";
+import { getCurrentUser } from "aws-amplify/auth/server";
+import { runWithAmplifyServerContext } from "@/utils/amplifyServerUtils";
+import { AmplifyServer } from "aws-amplify/adapter-core";
+
+import outputs from "@/../amplify_outputs.json";
+
+Amplify.configure(outputs, { ssr: true });
+
+export const checkAuthSession = async (): Promise<boolean> => {
+  try {
+    const { userId } = await runWithAmplifyServerContext({
+      nextServerContext: { cookies },
+      operation: (contextSpec: AmplifyServer.ContextSpec) =>
+        getCurrentUser(contextSpec),
+    });
+    if (userId != null || userId != undefined || userId != "") {
+      return true;
+    }
+  } catch {
+    return false;
+  }
+  return false;
+};
