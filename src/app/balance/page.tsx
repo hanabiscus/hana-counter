21de94dd0d057cf39bb1b13ebb6a6bd61caf866d
hanabiscus <hana-counter@outlook.jp>
2025-04-18 02:58:12 +0900

    不要コード削除

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,26 +1,14 @@
 import Balance from "@/components/page-components/Balance";
 import { checkAuthSession } from "@/server/authentication/amplifyAuthSession";
-import {
-  getBalanceMonthList,
-  getMonthlyBalanceData,
-} from "@/server/balance/balanceProcessors";
-import { getCurrentBalanceMonth } from "@/utils/dateUtils";
-import { balanceDTOType, balanceMonthDTOType } from "@/const/types";
+import { getBalanceMonthList } from "@/server/balance/balanceProcessors";
+import { balanceMonthDTOType } from "@/const/types";
 
 const BalancePage = async () => {
   const isLogin = await checkAuthSession();
 
-  const fetchedMonthlyBalanceData: balanceDTOType = [];
   const fetchedBalanceMonthData: balanceMonthDTOType = [];
 
-  const currentBalanceMonth = getCurrentBalanceMonth();
-
   if (isLogin) {
-    Array.prototype.push.apply(
-      fetchedMonthlyBalanceData,
-      await getMonthlyBalanceData(currentBalanceMonth)
-    );
-
     Array.prototype.push.apply(
       fetchedBalanceMonthData,
       await getBalanceMonthList()
