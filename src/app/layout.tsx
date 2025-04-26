8f440d66fc1c7e7cf3a54369bcd0e7c1afc3c322
hanabiscus <hana-counter@outlook.jp>
2025-04-26 22:54:16 +0900

    不要コード削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { JSX } from "react";
 import type { Metadata } from "next";
-import { fontNotoColorEmoji, fontNotoSansJp } from "@/utils/font";
+import { fontNotoSansJp } from "@/utils/font";
 import ConfigureAmplifyClientSide from "@/components/configure-amplify-client-side/ConfigureAmplifyClientSide";
 import SuperHeaderMenu from "@/components/headers/super-header-menu/SuperHeaderMenu";
 import Header from "@/components/headers/header/Header";
@@ -26,9 +26,7 @@ const RootLayout: ({
         <ConfigureAmplifyClientSide />
         <div className="z-50 opacity-100 sticky top-0">
           <SuperHeaderMenu />
-          <div className={`${fontNotoColorEmoji.variable}`}>
-            <Header />
-          </div>
+          <Header />
         </div>
         <main>{children}</main>
       </body>
--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,16 +1,9 @@
 import { NextFontWithVariable } from "next/dist/compiled/@next/font";
-import { Noto_Sans_JP, Noto_Color_Emoji } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 
 export const fontNotoSansJp: NextFontWithVariable = Noto_Sans_JP({
   weight: ["400", "500"],
   subsets: ["latin"],
   variable: "--font-Noto-Sans-JP",
-  display: "swap",
-});
-
-export const fontNotoColorEmoji: NextFontWithVariable = Noto_Color_Emoji({
-  weight: ["400"],
-  subsets: ["emoji"],
-  variable: "--font-Noto-Color-Emoji",
   display: "swap",
 });
